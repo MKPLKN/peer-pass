9e6b3d1598014ebfc04bc9d56fd86321d9ada703
MKPLKN <15703708+MKPLKN@users.noreply.github.com>
2024-02-02 14:57:17 +0200

    Implement listener manager

--- src/modules/event/index.js
+++ src/modules/event/index.js
@@ -2,16 +2,10 @@ const awilix = require('awilix')
 const EventService = require('./event-service')
 const ListenerManager = require('./listener-manager')
 
-class listenerFactory {
-  create () {
-    return new ListenerManager()
-  }
-}
-
 function registerModule (container) {
   container.register({
     eventService: awilix.asClass(EventService).singleton(),
-    listenerFactory: awilix.asClass(listenerFactory).singleton()
+    listenerManager: awilix.asClass(ListenerManager).singleton()
   })
 }
 
